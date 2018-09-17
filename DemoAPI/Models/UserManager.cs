8b1cbc1a34cbddd08144196b3bd9d1bc2bddab87
HAMISH LYALL <101614208@student.swin.edu.au>
2018-09-17 10:17:56 +1000

    Sem2BasicAuthSubTask

---
+++ DemoAPI/Models/UserManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class UserManager
+    {
+        private DemoDBEntities _Context;
+
+        public UserManager()
+        {
+            _Context = new DemoDBEntities();
+        }
+        public bool ValidateUser(string userName, string Password)
+        {
+            var result = _Context.UserMasters.SingleOrDefault(x => x.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase) && x.Password == Password);
+
+            return result != null ? true : false;
+        }
+    }
+}
